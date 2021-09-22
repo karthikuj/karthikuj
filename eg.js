7d3a450a4d2b06a81debb1afd6e3c16d7bb2adf1
Karthik U J <59091280+karthikuj@users.noreply.github.com>
2021-09-22 23:28:58 +0530

    Delete eg.js

--- eg.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var _old_alert = window.alert;
-    window.alert = function() {
-                     // run some code when the alert pops up
-        _old_alert.apply(window,arguments);
-                     // run some code after the alert
-        console.log("ACECTF{'this_is_an_easter_egg'}");
-    };
-})();
