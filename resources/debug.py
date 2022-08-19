29d66d0449f9d83d3472391c645580d331faff20
Karthik U J <59091280+karthikuj@users.noreply.github.com>
2022-08-19 17:25:58 +0530

    Update debug.py

--- resources/debug.py
+++ resources/debug.py
@@ -1,18 +1,19 @@
 import requests
 
-api = input('Enter api key')
-choice = input('Enter your choice:\n1. Load a script\n2. Enable a script')
+api = input('Enter api key: ')
+choice = input('Enter your choice:\n1. Load a script\n2. Enable a script: ')
 headers = {
   'Accept': 'application/json',
   'X-ZAP-API-Key': api
 }
 
 
+port = input('Enter port number: )
 scriptName = input('Enter script name: ')
 if choice == '1':
     scriptType = input('Enter script type: ')
     scriptPath = input('Enter script path: ')
-    r = requests.get('http://zap/JSON/script/action/load/', params={
+    r = requests.get(f'http://127.0.0.1{port}/JSON/script/action/load/', params={
     'scriptName': scriptName,  'scriptType': scriptType,  'scriptEngine': 'Oracle Nashorn',  'fileName': scriptPath
     }, headers = headers)
 elif choice == '2':
